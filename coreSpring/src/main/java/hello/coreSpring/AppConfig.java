dcd0113ad55a3dcc305052df9f291b8368627bda
jjwon915 <jjwon915@naver.com>
2021-10-05 00:47:33 +0900

    AppConfig.java - 스프링 기반으로 변경 - @Configuration : 설정 및 구성 정보의미 - @Bean : 스프링 컨테이너에 등록됨.

--- coreSpring/src/main/java/hello/coreSpring/AppConfig.java
+++ coreSpring/src/main/java/hello/coreSpring/AppConfig.java
@@ -9,21 +9,28 @@ import hello.coreSpring.member.MemberServiceImpl;
 import hello.coreSpring.member.MemoryMemberRepository;
 import hello.coreSpring.order.OrderService;
 import hello.coreSpring.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
