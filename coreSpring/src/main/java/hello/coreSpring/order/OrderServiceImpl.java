6cb9befa63f9a8594640cd56a17f8d8ba806d269
jjwon915 <jjwon915@naver.com>
2021-10-05 00:32:08 +0900

    AppConfig의 메소드 호출을 통해 AppConfig에서 생성하도록 변경. - 따라서 구현체가 무엇인지는 알 수 없고, 로직을 실행하기만 하면 된다.

--- coreSpring/src/main/java/hello/coreSpring/order/OrderServiceImpl.java
+++ coreSpring/src/main/java/hello/coreSpring/order/OrderServiceImpl.java
@@ -2,14 +2,20 @@ package hello.coreSpring.order;
 
 import hello.coreSpring.discount.DiscountPolicy;
 import hello.coreSpring.discount.FixDiscountPolicy;
+import hello.coreSpring.discount.RateDiscountPolicy;
 import hello.coreSpring.member.Member;
 import hello.coreSpring.member.MemberRepository;
 import hello.coreSpring.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- coreSpring/src/test/java/hello/coreSpring/order/OrderServiceTest.java
+++ coreSpring/src/test/java/hello/coreSpring/order/OrderServiceTest.java
@@ -1,16 +1,25 @@
 package hello.coreSpring.order;
 
+import hello.coreSpring.AppConfig;
 import hello.coreSpring.member.Grade;
 import hello.coreSpring.member.Member;
 import hello.coreSpring.member.MemberService;
 import hello.coreSpring.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder(){
