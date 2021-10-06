8e8f63aa14bb0a778049123227da45d8f921c3e3
jjwon915 <jjwon915@naver.com>
2021-10-07 00:25:53 +0900

    ApplicationContextBasicSearchTest.java - 스프링 빈 조회 - 구현체 타입으로 조회하는 것은 별로 좋지 않음. 유연성이 떨어짐.

---
+++ coreSpring/src/test/java/hello/coreSpring/beanSearch/ApplicationContextBasicSearchTest.java
@@ -0,0 +1,43 @@
+package hello.coreSpring.beanSearch;
+
+import hello.coreSpring.AppConfig;
+import hello.coreSpring.member.MemberService;
+import hello.coreSpring.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicSearchTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("타입으로 빈으로 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구현체 타입으로 조회")
+    void findBeanByImplType(){
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 불가")
+    void findBeanByNameX(){
+        org.junit.jupiter.api.Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class));
+    }
+}
